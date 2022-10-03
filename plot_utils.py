0eb1cac63379e01e27f8de3e269bd569be01d915
Gian Marco Falcone <gian.marco.1698@gmail.com>
2022-10-03 21:33:14 +0200

    added final comparison

--- plot_utils.py
+++ plot_utils.py
@@ -80,3 +80,39 @@ def print_cm(svm_cm, labels, title):
     plt.title(title)
     sns.heatmap(svm_cm, annot=True, linewidths=.5, ax=ax, 
             xticklabels=labels, yticklabels=labels, fmt='g', vmin=0, vmax=500)
+
+
+# Bar plot used to compare the best cases of the five classifier
+#
+# @param metrics_array_1: array for the first column
+# @param metrics_array_2: array for the second column
+# @param metrics_array_3: array for the third column
+# @param metrics_array_4: array for the fourth column
+# @param metrics_array_5: array for the fifth column
+# @param labels: labels array
+# @param title: chart title
+# @param xlabel: x-axis label
+# @param ylabel: y-axis label
+# @param legend: list with bar legend
+#
+def five_group_bar_plot(metrics_array_1, metrics_array_2, metrics_array_3, metrics_array_4, 
+        metrics_array_5, labels, 
+        title, xlabel, ylabel, legend):
+    plt.figure(figsize=(30,15))
+    X_axis = np.arange(len(labels))
+    width = 0.11  # the width of the bars
+
+
+    plt.bar(X_axis - 0.26, metrics_array_1, width, label="Support Vector Machine")
+    plt.bar(X_axis - 0.13, metrics_array_2, width, label="Logistic Regression")
+    plt.bar(X_axis, metrics_array_3, width, label="KNeighbors")
+    plt.bar(X_axis + 0.13, metrics_array_4, width, label="Multilayer Perceptron")
+    plt.bar(X_axis + 0.26, metrics_array_5, width, label="Decision Tree")
+
+
+    plt.xticks(X_axis, labels)
+
+    plt.legend(legend)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
